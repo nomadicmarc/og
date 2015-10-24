aee1fb7d4bc9253612a77a86e4d9222fde0500c6
Scott <scott@goodbay.co>
2015-10-23 17:34:18 -0700

    added twitter, churched up form, revised footer, added thank you page, blah blah blah

--- apply/index.php
+++ apply/index.php
@@ -33,9 +33,6 @@ if(isset($_POST['submit'])){
     <title>InternD - Apply for Escort Internship</title>
     <link rel="icon" type="image/png" href="../img/favicon.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
 
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
@@ -63,54 +60,65 @@ if(isset($_POST['submit'])){
 
 <body id="page-top">
 
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top affix">
+    <nav class="navbar navbar-default navbar-fixed-top affix">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand page-scroll" href="/dev/og">InternD</a>
+                <a class="navbar-brand" href="/dev/og">InternD</a>
             </div>
         </div>
     </nav>
 
 
 
-    <section class="bg-primary" id="about">
+    <section class="bg-secondary" id="about">
+        <div class="row form-header">
+            <div class="col-md-4 sm-center">
+                <img src="../img/id-apply.png" alt="" />
+            </div>
+            <div class="col-md-8 sm-center">
+                <h2 class="section-heading h2-apply">Apply Now</h2>
+                <p>The best D is an InternD, so fill out the form and get started today.</p>
+            </div>
+        </div>
+        <hr class="primary">
+
         <div class="container form-box">
-            <h2 class="section-heading text-center">Apply Now.</h2>
-            <hr class="secondary">
-            <form action="" method="post" enctype="multipart/form-data">
+
+
+            <form action="../thank-you" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                  <label class="text-faded" for="first-name">First Name:</label>
+                  <label class="form-text" for="first-name">First Name:</label>
                   <input type="text" class="form-control" id="first-name" name="first-name">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="last-name">Last Name:</label>
+                  <label class="form-text" for="last-name">Last Name:</label>
                   <input type="text" class="form-control" id="last-name" name="last-name">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="age">Age:</label>
+                  <label class="form-text" for="age">Age:</label>
                   <input type="text" class="form-control" id="age" name="age">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="address">Street Address:</label>
+                  <label class="form-text" for="address">Street Address:</label>
                   <input type="text" class="form-control" id="address" name="address">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="city">City:</label>
+                  <label class="form-text" for="city">City:</label>
                   <input type="text" class="form-control" id="city" name="city">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="address">State:</label>
+                  <label class="form-text" for="address">State:</label>
                   <input type="text" class="form-control" id="state" name="state">
                 </div>
                 <div class="form-group">
-                    <label class="text-faded" for="phone">Phone:</label>
+                    <label class="form-text" for="phone">Phone:</label>
                     <input type="text" class="form-control" data-format="+1 (ddd) ddd-dddd" id="phone" name="phone">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="address">Email:</label>
+                  <label class="form-text" for="address">Email:</label>
                   <input type="email" class="form-control" id="email" name="email">
                 </div>
-                <p class="text-faded">Can you provide your own transportation?</p>
+                <p class="form-text">Can you provide your own transportation?</p>
                 <div class="radio">
                   <label>
                     <input type="radio" name="transportation" id="transportation" value="yes" checked>
@@ -123,7 +131,7 @@ if(isset($_POST['submit'])){
                     No
                   </label>
                 </div>
-                <p class="text-faded">Are you male or female?</p>
+                <p class="form-text">Are you male or female?</p>
                 <div class="radio">
                   <label>
                     <input type="radio" name="gender" id="gender" value="male" checked>
@@ -137,7 +145,7 @@ if(isset($_POST['submit'])){
                   </label>
                 </div>
                 <div class="form-group">
-                    <label class="text-faded" for="height">
+                    <label class="form-text" for="height">
                         Height:
                     </label>
                     <select class="form-control" id="height" name="height">
@@ -182,15 +190,15 @@ if(isset($_POST['submit'])){
                     </select>
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="weight">Weight:</label>
+                  <label class="form-text" for="weight">Weight:</label>
                   <input type="text" class="form-control" id="weight" name="weight">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="waist">Waist Size:</label>
+                  <label class="form-text" for="waist">Waist Size:</label>
                   <input type="text" class="form-control" id="waist" name="waist">
                 </div>
                 <div class="form-group">
-                    <label class="text-faded" for="eye">
+                    <label class="form-text" for="eye">
                         Eye color:
                     </label>
                     <select class="form-control" id="eye" name="eye">
@@ -204,18 +212,19 @@ if(isset($_POST['submit'])){
                     </select>
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="portfolio">Link to your portfolio:</label>
+                  <label class="form-text" for="portfolio">Link to your portfolio:</label>
                   <input type="text" class="form-control" id="portfolio" name="portfolio">
                 </div>
                 <div class="form-group">
-                  <label class="text-faded" for="photo">Photo Upload</label>
+                  <label class="form-text" for="photo">Photo Upload</label>
                   <input type="file" id="photo" name="photo">
                   <p class="help-block">Please upload a recent photo.</p>
                 </div>
                 <div class="text-center">
-                    <input class="btn btn-default btn-xl wow tada" type="submit" name="submit" value="Submit">
+                    <input class="btn btn-default btn-xl btn-black wow tada" type="submit" name="submit" value="Apply">
                 </div>
             </form>
+            <p class="fine-print">By clicking “Apply” , you agree to InternD's Terms and Conditions and Privacy Policy.</p>
         </div>
 
     </section>
@@ -223,37 +232,63 @@ if(isset($_POST['submit'])){
     <section id="contact">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-lg-offset-2 text-center">
+                <div class="col-md-2 col-md-offset-3 text-center">
                     <i class="fa fa-phone fa-3x wow bounceIn"></i>
-                    <p><a href="mailto:contact@internd.com">Contact@internd.com</a></p>
+                    <p><a href="mailto:contact@internd.com">(702) 930-8469</a></p>
                 </div>
-                <div class="col-lg-4 text-center">
+                <div class="col-md-2 text-center">
+                    <i class="fa fa-twitter fa-3x wow bounceIn"></i>
+                    <p><a href="http://twitter.com/_internd">@internD</a></p>
+                </div>
+                <div class="col-md-2 text-center">
                     <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-                    <p><a href="mailto:feedback@internd.com">feedback@internd.com</a></p>
+                    <p><a href="mailto:contact@internd.com">contact@internd.com</a></p>
                 </div>
-                <div class="col-lg-8 col-lg-offset-2 text-center address">
-                    <p>InternD</p>
-                    <p>317 S 6th St</p>
-                    <p>Las Vegas, NV 89101</p>
-                </div>
+            </div>
+            <div class="col-lg-8 col-lg-offset-2 text-center address">
+                <p>InternD</p>
+                <p>317 S 6th St</p>
+                <p>Las Vegas, NV 89101</p>
             </div>
         </div>
     </section>
 
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="../js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/jquery.fittext.js"></script>
-    <script src="js/wow.min.js"></script>
+    <script src="../js/jquery.easing.min.js"></script>
+    <script src="../js/jquery.fittext.js"></script>
+    <script src="../js/wow.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="js/creative.js"></script>
+    <script src="../js/creative.js"></script>
 
+    <!-- Intercom JavaScript -->
+    <script>
+      window.intercomSettings = {
+        app_id: "jaq34i4y"
+      };
+    </script>
+    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/jaq34i4y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
+    <!-- Google Tracking Code -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-69227594-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    
 </body>
 </html>
